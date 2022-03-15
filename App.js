bdc130cc566a8aa7809ff0307eafdbe8a7d2b590
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-16 08:17:35 +0900

    09-1 useAssets, useFont 훅 사용

--- App.js
+++ App.js
@@ -3,34 +3,14 @@ import React, { useState } from 'react';
 import { Text, Image, ImageStore } from 'react-native';
 import * as Font from 'expo-font'
 import { Ionicons } from '@expo/vector-icons'
-import { Asset } from 'expo-asset';
+import { Asset, useAssets } from 'expo-asset';
 
-const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font))
-const loadImages = (images) => images.map(image => {
-  if (typeof image === "string") return Image.prefetch(image)
-  else return Asset.loadAsync(image)
-})
 export default function App() {
-  const [ready, setReady] = useState(false)
-  const onFinish = () => setReady(true)
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font])
-    const images = loadImages([
-      require('./screenshot.png'),
-      "https://bopboy.github.io/assets/img/me.png"
-    ])
-    await Promise.all([...fonts, ...images])
-    // console.log(images)
-    // await Asset.loadAsync(require('./screenshot.png'))
-    // await Image.prefetch("https://bopboy.github.io/assets/img/me.png")
-  }
-  if (!ready) {
+  const [assets] = useAssets([require("./screenshot.png")])
+  const [loaded] = Font.useFonts(Ionicons.font)
+  if (!assets || !loaded) {
     return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
+      <AppLoading />
     )
   }
   return (
