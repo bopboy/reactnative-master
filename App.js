fb38865f21be23b4cd158b3ba0b741fdb4372c74
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-16 08:09:22 +0900

    09 AppLoading (함수로 처리)

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import AppLoading from 'expo-app-loading';
 import React, { useState } from 'react';
-import { Text, Image } from 'react-native';
+import { Text, Image, ImageStore } from 'react-native';
 import * as Font from 'expo-font'
 import { Ionicons } from '@expo/vector-icons'
 import { Asset } from 'expo-asset';
 
+const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font))
+const loadImages = (images) => images.map(image => {
+  if (typeof image === "string") return Image.prefetch(image)
+  else return Asset.loadAsync(image)
+})
 export default function App() {
   const [ready, setReady] = useState(false)
   const onFinish = () => setReady(true)
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font)
-    await Asset.loadAsync(require('./screenshot.png'))
-    await Image.prefetch("https://bopboy.github.io/assets/img/me.png")
+    const fonts = loadFonts([Ionicons.font])
+    const images = loadImages([
+      require('./screenshot.png'),
+      "https://bopboy.github.io/assets/img/me.png"
+    ])
+    await Promise.all([...fonts, ...images])
+    // console.log(images)
+    // await Asset.loadAsync(require('./screenshot.png'))
+    // await Image.prefetch("https://bopboy.github.io/assets/img/me.png")
   }
   if (!ready) {
     return (
