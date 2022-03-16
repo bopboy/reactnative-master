c20a3341667e4c6973ec5592f7d3241c30d7d760
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-16 19:09:14 +0900

    17 스택 네비게이션 커스터마이징 연습

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { View, Text, TouchableOpacity } from 'react-native'
+import { YELLOW_COLOR } from '../colors'
 
 const NavigateStack = createNativeStackNavigator()
 
@@ -10,19 +11,29 @@ const ScreenOne = ({ navigation: { navigate } }) => (
 const ScreenTwo = ({ navigation: { navigate } }) => (
     <TouchableOpacity onPress={() => navigate("Three")}><Text>go to Three</Text></TouchableOpacity>
 )
-const ScreenThree = ({ navigation: { setOptions } }) => (
-    <TouchableOpacity onPress={() => setOptions({ title: "Hello" })}><Text>go to Back (Two)</Text></TouchableOpacity>
-)
-// const ScreenThree = ({ navigation: { goBack } }) => (
-//     <TouchableOpacity onPress={() => goBack()}><Text>go to Back (Two)</Text></TouchableOpacity>
+// const ScreenThree = ({ navigation: { setOptions } }) => (
+//     <TouchableOpacity onPress={() => setOptions({ title: "Hello" })}><Text>go to Back (Two)</Text></TouchableOpacity>
 // )
+const ScreenThree = ({ navigation: { goBack } }) => (
+    <TouchableOpacity onPress={() => goBack()}><Text>go to Back (Two)</Text></TouchableOpacity>
+)
 
 const Stack = () => {
     return (
-        <NavigateStack.Navigator>
+        <NavigateStack.Navigator
+            screenOptions={{
+                headerBackTitleVisible: false,
+                headerShown: true,
+                headerTintColor: YELLOW_COLOR,
+                presentation: "card",
+                animation: "slide_from_right"
+            }}
+        >
             <NavigateStack.Screen name="One" component={ScreenOne} />
             <NavigateStack.Screen name="Two" component={ScreenTwo} />
-            <NavigateStack.Screen name="Three" component={ScreenThree} />
+            <NavigateStack.Screen name="Three" component={ScreenThree}
+                options={{ presentation: "modal" }}
+            />
         </NavigateStack.Navigator>
     )
 }
