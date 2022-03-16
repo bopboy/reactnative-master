919488dded8ef5f93634bc2a795609b5227851a5
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-16 14:47:24 +0900

    14 탭 바 아이콘

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -4,7 +4,8 @@ import Movies from '../screens/Movies'
 import Search from '../screens/Search'
 import Tv from '../screens/Tv'
 import { useColorScheme } from 'react-native'
-import { BLACK_COLOR, YELLOW_COLOR } from '../colors'
+import { BLACK_COLOR, DARK_GREY, LIGHT_GERY, WHITE_COLOR, YELLOW_COLOR } from '../colors'
+import { Ionicons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator()
 
@@ -13,16 +14,43 @@ const Tabs = () => {
     return (
         <Tab.Navigator
             screenOptions={{
-                tabBarStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
+                tabBarStyle: { backgroundColor: isDark ? BLACK_COLOR : WHITE_COLOR },
                 tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
-                tabBarInactiveTintColor: isDark ? "#d2dae2" : "#808e9b",
-                headerStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
-                headerTitleStyle: { color: isDark ? "white" : BLACK_COLOR }
+                tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GERY,
+                headerStyle: { backgroundColor: isDark ? BLACK_COLOR : WHITE_COLOR },
+                headerTitleStyle: { color: isDark ? WHITE_COLOR : BLACK_COLOR },
+                tabBarLabelStyle: {
+                    marginTop: -5, fontSize: 12, fontWeight: "600"
+                }
             }}
         >
-            <Tab.Screen name="Movies" component={Movies} />
-            <Tab.Screen name="Tv" component={Tv} />
-            <Tab.Screen name="Search" component={Search} />
+            <Tab.Screen
+                name="Movies" component={Movies}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => {
+                        console.log(focused, color, size)
+                        return <Ionicons name="film-outline" color={color} size={size} />
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="TV" component={Tv}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => {
+                        console.log(focused, color, size)
+                        return <Ionicons name="tv-outline" color={color} size={size} />
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="Search" component={Search}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => {
+                        console.log(focused, color, size)
+                        return <Ionicons name="search-outline" color={color} size={size} />
+                    }
+                }}
+            />
         </Tab.Navigator>
     )
 }
