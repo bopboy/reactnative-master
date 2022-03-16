8a7ab4b513009111886c2fa43a12eaa264c0ea2f
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-16 11:57:31 +0900

    12 탭 네비게이션 커스터마이징 연습

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,13 +3,33 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Movies from '../screens/Movies'
 import Search from '../screens/Search'
 import Tv from '../screens/Tv'
+import { View, Text } from 'react-native'
 
 const Tab = createBottomTabNavigator()
 
 const Tabs = () => (
-    <Tab.Navigator>
+    <Tab.Navigator
+        initialRouteName='Search'
+        screenOptions={{
+            tabBarLabelStyle: { backgroundColor: "red" },
+            tabBarActiveTintColor: "red",
+            tabBarInactiveTintColor: "skyblue",
+            tabBarStyle: { backgroundColor: "yellow" },
+            headerTitleStyle: { color: "tomato" }
+        }}
+    >
         <Tab.Screen name="Movies" component={Movies} />
-        <Tab.Screen name="Tv" component={Tv} />
+        <Tab.Screen name="Tv"
+            component={Tv}
+            options={{
+                tabBarLabelStyle: { backgroundColor: "blue" },
+                tabBarBadge: 5,
+                headerRight: () => (
+
+                    <View><Text>Hello</Text></View>
+                )
+            }}
+        />
         <Tab.Screen name="Search" component={Search} />
     </Tab.Navigator>
 )
