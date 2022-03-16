b12c6c3c6cd81df01cb72a9d20d3694c69dc64b6
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-16 13:38:26 +0900

    13 다크/라이트 모드

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,34 +3,27 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Movies from '../screens/Movies'
 import Search from '../screens/Search'
 import Tv from '../screens/Tv'
-import { View, Text } from 'react-native'
+import { useColorScheme } from 'react-native'
+import { BLACK_COLOR, YELLOW_COLOR } from '../colors'
 
 const Tab = createBottomTabNavigator()
 
-const Tabs = () => (
-    <Tab.Navigator
-        initialRouteName='Search'
-        screenOptions={{
-            tabBarLabelStyle: { backgroundColor: "red" },
-            tabBarActiveTintColor: "red",
-            tabBarInactiveTintColor: "skyblue",
-            tabBarStyle: { backgroundColor: "yellow" },
-            headerTitleStyle: { color: "tomato" }
-        }}
-    >
-        <Tab.Screen name="Movies" component={Movies} />
-        <Tab.Screen name="Tv"
-            component={Tv}
-            options={{
-                tabBarLabelStyle: { backgroundColor: "blue" },
-                tabBarBadge: 5,
-                headerRight: () => (
-
-                    <View><Text>Hello</Text></View>
-                )
+const Tabs = () => {
+    const isDark = useColorScheme() === "dark"
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
+                tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+                tabBarInactiveTintColor: isDark ? "#d2dae2" : "#808e9b",
+                headerStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
+                headerTitleStyle: { color: isDark ? "white" : BLACK_COLOR }
             }}
-        />
-        <Tab.Screen name="Search" component={Search} />
-    </Tab.Navigator>
-)
+        >
+            <Tab.Screen name="Movies" component={Movies} />
+            <Tab.Screen name="Tv" component={Tv} />
+            <Tab.Screen name="Search" component={Search} />
+        </Tab.Navigator>
+    )
+}
 export default Tabs
