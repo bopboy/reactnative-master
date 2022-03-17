9905717f217f60d4e9c056e81d082533793c1d9b
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-17 20:44:41 +0900

    28 리팩토링, 데이터 fetch (상영 예정, 트렌딩)

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -2,16 +2,12 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import React, { useEffect, useState } from 'react'
 import Swiper from 'react-native-swiper'
 import styled from 'styled-components/native'
-import { ActivityIndicator, Dimensions, StyleSheet, useColorScheme } from 'react-native'
-import { BlurView } from 'expo-blur'
+import { ActivityIndicator, Dimensions } from 'react-native'
 import { TMDB_API_KEY } from '../config'
-import { makeImagePath } from '../utils'
+import Slide from '../components/Slide'
 
 const BASE_PATH = "https://api.themoviedb.org/3";
 const Container = styled.ScrollView`
-`
-const View = styled.View`
-    flex: 1;
 `
 const Loader = styled.View`
     flex: 1;
@@ -19,47 +15,30 @@ const Loader = styled.View`
     align-items: center;
     background-color: ${props => props.theme.mainBgColor};
 `
-const BgImg = styled.Image``
-const Poster = styled.Image`
-    width: 100px;
-    height: 160px;
-    border-radius: 5px;
-`
-const Title = styled.Text`
-    font-size: 16px;
-    font-weight: 600;
-    color:white;
-`
-const Wrapper = styled.View`
-    flex-direction: row;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
-`
-const Column = styled.View`
-    width: 40%;
-    margin-left: 15px;
-`
-const Overview = styled.Text`
-    margin-top: 10px;
-    color: rgba(255, 255, 255, 0.8);
-`
-const Votes = styled(Overview)`
-    font-size: 12px;
-`
-
 const { height: SCREEN_HEIGHT } = Dimensions.get("window")
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({ navigation: { navigate } }) => {
-    const isDark = useColorScheme() === "dark"
     const [loading, setLoading] = useState(true)
     const [nowPlaying, setNowPlaying] = useState([])
+    const [upcoming, setUpcoming] = useState([])
+    const [trending, setTrending] = useState([])
+    const getTrending = async () => {
+        const { results } = await (await (fetch(`${BASE_PATH}/trending/movie/week?api_key=${TMDB_API_KEY}`))).json()
+        setTrending(results)
+    }
+    const getUpcoming = async () => {
+        const { results } = await (await (fetch(`${BASE_PATH}/movie/upcoming?api_key=${TMDB_API_KEY}&page=1&region=kr`))).json()
+        setUpcoming(results)
+    }
     const getNowPlaying = async () => {
-        const { results } = await (await (fetch(`${BASE_PATH}/movie/now_playing?api_key=${TMDB_API_KEY}&region=kr`))).json()
+        const { results } = await (await (fetch(`${BASE_PATH}/movie/now_playing?api_key=${TMDB_API_KEY}&page=1&region=kr`))).json()
         setNowPlaying(results)
+    }
+    const getData = async () => {
+        await Promise.all([getTrending(), getUpcoming(), getNowPlaying()])
         setLoading(false)
     }
     useEffect(() => {
-        getNowPlaying()
+        getData()
     }, [])
     return loading ?
         (<Loader><ActivityIndicator size="large" color="#00ff00" /></Loader>) :
@@ -75,28 +54,14 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({ navigation: {
                     containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 3 }}
                 >
                     {nowPlaying.map(movie => (
-                        <View key={movie.id}>
-                            <BgImg
-                                style={StyleSheet.absoluteFill}
-                                source={{ uri: makeImagePath(movie.backdrop_path) }}
-                            />
-                            <BlurView
-                                intensity={100}
-                                style={StyleSheet.absoluteFill}
-                                tint={isDark ? "light" : "dark"}
-                            >
-                                <Wrapper>
-                                    <Poster source={{ uri: makeImagePath(movie.poster_path) }} />
-                                    <Column>
-                                        <Title>{movie.original_title}</Title>
-                                        <Overview>{movie.overview.slice(0, 90)}...</Overview>
-                                        {movie.vote_average > 0 ?
-                                            (<Votes>⭐️ {movie.vote_average} / 10</Votes>) : null
-                                        }
-                                    </Column>
-                                </Wrapper>
-                            </BlurView>
-                        </View>
+                        <Slide
+                            key={movie.id}
+                            backdropPath={movie.backdrop_path}
+                            posterPath={movie.poster_path}
+                            originalTitle={movie.original_title}
+                            voteAverage={movie.vote_average}
+                            overview={movie.overview}
+                        />
                     ))}
                 </Swiper>
             </Container>
