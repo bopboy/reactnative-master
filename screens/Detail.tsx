201a13d116bf2655b16df6b8e4d3d775207f2af8
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-20 12:03:34 +0900

    48 Detail 스크린 5 (비디오 링크 처리)

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,14 +1,17 @@
+import { Ionicons } from '@expo/vector-icons'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { LinearGradient } from 'expo-linear-gradient'
 import React from 'react'
 import { useEffect } from 'react'
-import { Dimensions, StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet, Linking } from 'react-native'
 import { useQuery } from 'react-query'
 import styled from 'styled-components/native'
 import { Movie, moviesApi, TV, tvApi } from '../api'
 import { BLACK_COLOR } from '../colors'
+import Loader from '../components/Loader'
 import Poster from '../components/Poster'
 import { makeImagePath } from '../utils'
+import * as WebBrowser from 'expo-web-browser'
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window")
 const Container = styled.ScrollView`
@@ -31,23 +34,41 @@ const Title = styled.Text`
     margin-left: 15px;
     font-weight: 500;
 `
+const Data = styled.View`
+    padding: 0 20px;
+`
 const Overview = styled.Text`
     color: ${props => props.theme.textColor};
-    margin-top: 20px;
-    padding: 0 20px;
+    margin: 20px 0;
+`
+const VideoBtn = styled.TouchableOpacity`
+    flex-direction: row;
+`
+const BtnText = styled.Text`
+    color: black;
+    font-weight: 600;
+    margin-bottom: 10px;
+    line-height: 24px;
+    margin-left: 10px;
 `
 type RootStackParamList = { Detail: Movie | TV }
 type DetailScreenProps = NativeStackScreenProps<RootStackParamList, "Detail">
 
 const Detail: React.FC<DetailScreenProps> =
     ({ navigation: { setOptions }, route: { params } }) => {
+        const isMovie = "original_title" in params
+        const { isLoading, data } = useQuery(
+            [isMovie ? "movies" : "tv", params.id],
+            isMovie ? moviesApi.detail : tvApi.detail,
+        )
         useEffect(() => {
             setOptions({ title: "original_title" in params ? "Movie" : "TV Show" })
         }, [])
-        const { isLoading: moviesLoading, data: moviesData } = useQuery(["movies", params.id], moviesApi.detail, { enabled: "original_title" in params })
-        const { isLoading: tvLoading, data: tvData } = useQuery(["tv", params.id], tvApi.detail, { enabled: "original_name" in params })
-        console.log("movies", moviesData)
-        console.log("tv", tvData)
+        const openYTLink = async (vidoeId: string) => {
+            const baseUrl = `https://m.youtube.com/watch?v=${vidoeId}`
+            // await Linking.openURL(baseUrl)
+            await WebBrowser.openBrowserAsync(baseUrl)
+        }
         return (
             <Container>
                 <Header>
@@ -66,7 +87,16 @@ const Detail: React.FC<DetailScreenProps> =
                         </Title>
                     </Column>
                 </Header>
-                <Overview>{params.overview}</Overview>
+                <Data>
+                    <Overview>{params.overview}</Overview>
+                    {isLoading ? <Loader /> : null}
+                    {data?.videos?.results?.map(video => (
+                        <VideoBtn key={video.key} onPress={() => openYTLink(video.key)}>
+                            <Ionicons name="logo-youtube" color="black" size={24} />
+                            <BtnText>{video.name}</BtnText>
+                        </VideoBtn>
+                    ))}
+                </Data>
             </Container>
         )
     }
